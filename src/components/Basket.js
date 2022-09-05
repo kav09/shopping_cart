bcd8521b9c18227ddce4ed4f71ca12b72723d241
Kavya Srivastava <kslovely.ks@gmail.com>
2022-09-05 14:31:36 +0530

    .

---
+++ src/components/Basket.js
@@ -0,0 +1,5 @@
+
+export default function Basket(){
+    return <div className="block col-2">Basket
+    </div>;
+}
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+
+export default function Main(props){
+
+    const { products } = props;
+
+    return(
+        <div className="block col-2">
+            <h2>Products</h2>
+            <div className="row">
+                {products.map((products) => (
+                        <div>{products.name}</div>
+                    ))}
+            </div>
+
+        </div>
+    );
+}
