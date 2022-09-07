b1dfd7692265796c5236eb2f6d0f536fd337480e
Kavya Srivastava <kslovely.ks@gmail.com>
2022-09-07 13:45:44 +0530

    Done

--- README.md
+++ README.md
@@ -13,3 +13,23 @@
 3. Create Main components
   1.  create data.js
   2. render product items
+
+4. Create Product component
+  1. create product divs
+  2. pass props to product component
+
+5. Implement cart 
+  1. add and remove items from the cart
+
+6. create basket component
+ 1. list items in cartItems
+ 2. calculate sub total
+ 3. show checkout button1
+
+7. save cart items in local storage
+ 1. save items in local storage on add and remove
+ 2. use useEffect to get items in local storage 
+
+8. use useTransition and useDeferedValue
+ 1. read items in local storage using useTransition
+ 2. change cartItems.length to  transition effect 
--- src/App.js
+++ src/App.js
@@ -2,17 +2,74 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Basket from "./components/Basket";
 import data from "./data";
+import { useDeferredValue, useEffect, useState, useTransition } from "react";
+//import { useTransition } from "react";
 
 function App() {
   
-  return (
-    <div className="App">
-      <Header/>
+  const [cartItems, setCartItems] = useState([]);
+  const {products} = data;
+  
+  //---------------------------------------------ONADD
+  
+  const onAdd = (product) => {
+    
+    const exist = cartItems.find((x) => x.id === product.id); 
+    if (exist)
+    {
+      const newCartItems = cartItems.map((x) => 
+      x.id === product.id ? {...exist, qty: exist.qty +1} :x
+      );
+      setCartItems(newCartItems);
+      //localstorage
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
+    }
+    else{
+      const newCartItems = [...cartItems, {...product, qty: 1}];
+      setCartItems(newCartItems);
+      //local storage
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
+    }
+  };
+  
+  //--------------------------------------------ONREMOVE-------
+
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if(exist.qty === 1){
+      const newCartItems = cartItems.filter((x) => x.id !== product.id);
+      setCartItems(newCartItems);
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
+    }
+    else{
+      const newCartItems = cartItems.map((x) => x.id === product.id ? {...exist, qty: exist.qty - 1} : x);
+      setCartItems(newCartItems);
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
+    }
+   };
+   
+   const[isPending, startTransition] = useTransition();
+
+   useEffect(() => {
+    startTransition( () => {setCartItems(
+      localStorage.getItem('cartItems') 
+      ? JSON.parse(localStorage.getItem('cartItems')):[]);});
+     },[]);
+
+
+     const cartItemsCount = useDeferredValue(cartItems.length);
+  
+      return isPending ? (<div>Loading...</div>): (
+    <div>
+      <Header countCartItems={cartItemsCount} />
       <div className="row">
-        <Main/>
-        <Basket/>
+        <Main cartItems={cartItems} 
+              onAdd={onAdd} 
+              onRemove={onRemove} 
+              products= {products}/>
+        <Basket cartItems={cartItems} onAdd={onAdd} onRemove={onRemove}/>
       </div>
-    </div>
+    </div>  
   );
 }
 
